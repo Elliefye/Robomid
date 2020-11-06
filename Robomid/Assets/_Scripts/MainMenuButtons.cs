4a67c106393324df7b3c7c7f0a6fb4d0d64edc34
Elina <elina815@gmail.com>
2020-11-06 19:03:44 +0200

    main menu

---
+++ Robomid/Assets/_Scripts/MainMenuButtons.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public GameObject MainButtons;
+    public GameObject OptionsMenu;
+
+    [SerializeField]
+    private Slider volumeSlider;
+
+    public void PlayClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OptionsClicked()
+    {
+        MainButtons.SetActive(false);
+        OptionsMenu.SetActive(true);
+        //set volume slider according to save file data
+        //volumeSlider.value = Game.current.Volume;
+    }
+
+    public void ExitClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        //save value to save file
+    }
+
+    public void BackClicked()
+    {
+        //save game
+        OptionsMenu.SetActive(false);
+        MainButtons.SetActive(true);
+    }
+}
